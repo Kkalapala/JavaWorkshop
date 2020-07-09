381c1db50448b3f1a26d5760d9c4d3a5f2009c18
kkalapala <kavya.kalapala@cloudium.io>
2020-07-09 15:51:45 +0530

    nested statements with logical operators

---
+++ src/ChapterTwo/example205.java
@@ -0,0 +1,29 @@
+package ChapterTwo;
+
+//checking normal heart rates while exercising
+
+public class example205 {
+    public static void main(String[] args) {
+        int age = 30;
+        int bpm = 150;
+
+        //complex conditionals nested statements
+        if (age == 30) {
+            if ((bpm >= 95) && (bpm <= 162)) {
+                System.out.println("Heart rate is normal");
+            } else if (bpm < 95) {
+                System.out.println("Heart rate is very low");
+            } else {
+                System.out.println("Heart rate is very high");
+            }
+        } else if (age == 60) {
+            if ((bpm >= 80) && (bpm <= 136)) {
+                System.out.println("Heart rate is normal");
+            } else if (bpm < 80) {
+                System.out.println("Heart rate is very low");
+            } else {
+                System.out.println("Heart rate is very high");
+            }
+        }
+    }
+}
