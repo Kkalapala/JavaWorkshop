fc65d120f78880ec0a0ed521eb7c8e1088900920
kkalapala <kavya.kalapala@cloudium.io>
2020-07-09 12:59:24 +0530

    if else loop

---
+++ src/ChapterTwo/example201.java
@@ -0,0 +1,22 @@
+package ChapterTwo;
+
+public class example201 {
+    public static void main(String[] args) {
+        boolean happy = true;
+        boolean sad = false;
+
+//        if (happy) {
+//            System.out.println("I am happy.");
+//        }
+//        if (sad) {
+//            //will not be seen
+//            System.out.println("I am sad");
+//        }
+//
+        if (happy) {
+            System.out.println("I am happy");
+        } else {
+            System.out.println("I am sad");
+        }
+    }
+}
