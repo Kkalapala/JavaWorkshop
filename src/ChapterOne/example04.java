6ba1e8e52f19cf8880066929294a499bc3d60d4b
kkalapala <kavya.kalapala@cloudium.io>
2020-07-09 12:18:57 +0530

    Added example 4 and example 5

---
+++ src/ChapterOne/example04.java
@@ -0,0 +1,14 @@
+package ChapterOne;
+
+// create empty variable of String type and modify value as program is running
+
+public class example04 {
+    public static void main(String[] args) {
+        String t = null;
+        System.out.println(t);
+        t = "Joe...";
+        System.out.println(t);
+        t = "went fishing";
+        System.out.println(t);
+    }
+}
---
+++ src/ChapterOne/example05.java
@@ -0,0 +1,14 @@
+package ChapterOne;
+
+// add different parts of string together
+
+public class example05 {
+    public static void main(String[] args) {
+        String t = null;
+        System.out.println(t);
+        t = t + "Joe...";
+        System.out.println(t);
+        t = t + "Joe...went fishing";
+        System.out.println(t);
+    }
+}
