5289d94c359042c0239e702356a67e6edf37a83e
kkalapala <kavya.kalapala@cloudium.io>
2020-07-09 17:52:51 +0530

    for loop 2

---
+++ src/ChapterTwo/example212.java
@@ -0,0 +1,14 @@
+package ChapterTwo;
+
+// enhanced for loop
+
+public class example212 {
+    public static void main(String[] args) {
+        String [] letters = { "A", "B", "C"};
+
+        //for loop
+        for (String letter : letters) {
+            System.out.println(letter);
+        }
+    }
+}
