99e92a8b3bf457cf218e30ebf3451150a0f6e2da
kkalapala <kavya.kalapala@cloudium.io>
2020-07-09 16:45:09 +0530

    equals method

---
+++ src/ChapterTwo/example208.java
@@ -0,0 +1,30 @@
+package ChapterTwo;
+
+//equals method
+
+public class example208 {
+    public static void main(String[] args) {
+        String cat = new String("cat");
+        String dog = new String("dog");
+
+        if (cat.equals(dog)) {
+            System.out.println("Cats and dogs are the same.");
+        } else if (!cat.equals(dog)) {
+            System.out.println("Cats and dogs are not the same.");
+        }
+
+        if(dog.equals(dog)) {
+            System.out.println("Dogs are dogs.");
+        }
+
+        //using literal strings
+        if (dog.equals("dog")) {
+            System.out.println("Dogs are dogs.");
+        }
+
+        //can compare using a literal string as well
+        if ("dog".equals(dog)) {
+            System.out.println("Dogs are dogs.");
+        }
+    }
+}
