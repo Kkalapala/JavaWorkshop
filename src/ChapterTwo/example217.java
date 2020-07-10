af3d5fb9d04d5fe9d3f428d966508fec1a1a83d2
kkalapala <kavya.kalapala@cloudium.io>
2020-07-10 14:28:11 +0530

    static

---
+++ src/ChapterTwo/example217.java
@@ -0,0 +1,12 @@
+package ChapterTwo;
+
+//static variables
+
+public class example217 {
+    //static variables used as constant. To keep them constant, use final
+    public static final String MULTIPLY = "multiply";
+
+    public static void main(String[] args) {
+        System.out.println("The operation is : " + MULTIPLY);
+    }
+}
