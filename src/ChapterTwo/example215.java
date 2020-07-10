b6d82182c48f433eeb80d2c53708599f9e4288b3
kkalapala <kavya.kalapala@cloudium.io>
2020-07-10 14:15:57 +0530

    example one

---
+++ src/ChapterTwo/example215.java
@@ -0,0 +1,14 @@
+package ChapterTwo;
+
+//run from command line
+
+public class example215 {
+    public static void main(String[] args) {
+        String[] animals = {"cat", "dog", "wombat"};
+        
+        //for loop
+        for (int i =0; i < animals.length; i++) {
+            System.out.println(i + " " + animals[i]);
+        }
+    }
+}
