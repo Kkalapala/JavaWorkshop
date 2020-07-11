0c5c871100eeff868601e51ad164540cc418d84d
kkalapala <kavya.kalapala@cloudium.io>
2020-07-11 12:38:14 +0530

    java arrays

---
+++ src/ChapterFour/example401.java
@@ -0,0 +1,17 @@
+package ChapterFour;
+
+import java.util.Arrays;
+
+public class example401 {
+    public static void main(String[] args) {
+        String[] text = new String[] { "spam", "more", "buy" };
+
+        Arrays.sort(text);
+
+        System.out.println(Arrays.toString(text));
+
+        for (int i = 0; i < text.length; i++) {
+            System.out.print(text[i] + " ");
+        }
+    }
+}
