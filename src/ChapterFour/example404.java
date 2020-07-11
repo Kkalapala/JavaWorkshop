d64268c9f23062b68b17b1522813054063654b5c
kkalapala <kavya.kalapala@cloudium.io>
2020-07-11 17:05:57 +0530

    binary search

--- src/ChapterFour/example404.java
+++ src/ChapterFour/example404.java
@@ -6,6 +6,8 @@ public class example404 {
     public static void main(String[] args) {
         String[] text = {"love", "is", "in", "the", "air"};
 
+        Arrays.sort(text);
+
         int search = Arrays.binarySearch(text, "is");
         System.out.println(search);
     }
