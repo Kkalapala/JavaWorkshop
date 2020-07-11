1dc2cf354124eae8da690b8a557b2d127f04a28b
kkalapala <kavya.kalapala@cloudium.io>
2020-07-11 17:51:39 +0530

    difference between two sets

---
+++ src/ChapterFour/example412.java
@@ -0,0 +1,28 @@
+package ChapterFour;
+
+import java.util.*;
+
+//difference between sets
+public class example412 {
+    public static void main(String[] args) {
+        Set set1 = new LinkedHashSet();
+
+        set1.add(35);
+        set1.add(19);
+        set1.add(11);
+        set1.add(83);
+        set1.add(7);
+
+        Set set2 = new LinkedHashSet();
+
+        set2.add(3);
+        set2.add(19);
+        set2.add(11);
+        set2.add(0);
+        set2.add(7);
+
+        set1.removeAll(set2);
+
+        System.out.println(set1);
+    }
+}
