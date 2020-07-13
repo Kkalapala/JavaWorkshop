14860c9442cc87f4a9b59d529b65f407eec32aa6
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 19:27:22 +0530

    fixing code of example504 to prevent exception

---
+++ src/ChapterFive/example505.java
@@ -0,0 +1,14 @@
+package ChapterFive;
+
+//fixing exception from example504
+public class example505 {
+    public static void main(String[] args) {
+        String vehicleType = null;
+        String vehicle = "car";
+        if (vehicle.equals(vehicleType)) {
+            System.out.println("it's a car");
+        } else {
+            System.out.println("it's not a car");
+        }
+    }
+}
