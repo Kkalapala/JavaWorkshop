14784fa7ad548f2466f800912f88f1a433d0fc0f
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 19:17:34 +0530

    IOexception

---
+++ src/ChapterFive/example503.java
@@ -0,0 +1,22 @@
+package ChapterFive;
+
+import java.nio.file.*;
+import java.util.*;
+
+public class example503 {
+    public static void main(String[] args) {
+        //declare a list that will contain all your files
+        //inside of a readme.txt file
+        List<String> lines = Collections.emptyList();
+
+        //provoke an exception
+       // lines = Files.readAllLines(Paths.get("readme.txt")); uncomment this line 
+
+        //this print will never be seen
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+
