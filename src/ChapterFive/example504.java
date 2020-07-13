3526ac74c243daf9aee2975b79f3ebe2f0a23361
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 19:23:31 +0530

    nullpointerexception

---
+++ src/ChapterFive/example504.java
@@ -0,0 +1,13 @@
+package ChapterFive;
+
+public class example504 {
+    public static void main(String[] args) {
+        String vehicleType = null;
+        String vehicle = "car";
+        if (vehicleType.equals(vehicle)) {
+            System.out.println("it's a car");
+        } else {
+            System.out.println("it's not a car");
+        }
+    }
+}
