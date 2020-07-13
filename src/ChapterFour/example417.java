33a6c973aeaf5299c84c3a87d6238d719039dc56
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 16:58:55 +0530

    maps

---
+++ src/ChapterFour/example417.java
@@ -0,0 +1,18 @@
+package ChapterFour;
+
+import java.util.*;
+
+public class example417 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("number", new Integer(1));
+        map.put("text", new String("hola"));
+        map.put("decimal", new Double(5.7));
+
+        System.out.println(map.get("text"));
+
+        if (!map.containsKey("byte")) {
+            System.out.println("There are no bytes here.");
+        }
+    }
+}
