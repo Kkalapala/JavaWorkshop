62fa050df666d4c13600328ee1a1637de9797c37
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 18:37:12 +0530

    enumeration list

---
+++ src/ChapterFour/example424.java
@@ -0,0 +1,19 @@
+package ChapterFour;
+
+import java.util.*;
+
+public class example424 {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        properties.put("OS", "Ubuntu Linux");
+        properties.put("version", "18.04");
+        properties.put("language", "English(UK)");
+
+        Enumeration enumeration = properties.propertyNames();
+
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
+    }
+}
