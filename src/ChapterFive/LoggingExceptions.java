0d72114da3fecee93dcd267d9e718a4e28b65fc8
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 20:20:14 +0530

    logging exceptions

---
+++ src/ChapterFive/LoggingExceptions.java
@@ -0,0 +1,17 @@
+package ChapterFive;
+
+import java.util.logging.*;
+
+public class LoggingExceptions {
+    public static void main(String[] args) {
+        Logger logger = Logger.getAnonymousLogger();
+
+        String s = null;
+
+        try {
+            System.out.println(s.length());
+        } catch (NullPointerException ne) {
+            logger.log(Level.SEVERE, "Exception happened.", ne);
+        }
+    }
+}
