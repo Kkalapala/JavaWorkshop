4f17dcf1abc09ae4207f92420b5066d04c18fd7f
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 17:22:06 +0530

    iterating through lists

---
+++ src/ChapterFour/example419.java
@@ -0,0 +1,28 @@
+package ChapterFour;
+
+import java.util.*;
+
+public class example419 {
+    public static void main(String[] args) {
+        List<Double> array = new ArrayList();
+        array.add(5.0);
+        array.add(2.2);
+        array.add(37.5);
+        array.add(3.1);
+        array.add(1.3);
+
+        System.out.println("Original List : " + array);
+
+        ListIterator listIterator = array.listIterator();
+
+        while (listIterator.hasNext()) {
+            //point to next element
+            double d = (Double) listIterator.next();
+
+            //round up the decimal number
+            listIterator.set(Math.round(d));
+        }
+
+        System.out.println("Modified List : " + array);
+    }
+}
