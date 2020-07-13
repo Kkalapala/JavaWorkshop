8629045db5aca27f97126f3498570c69c3314495
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 20:45:33 +0530

    finally block

---
+++ src/ChapterFive/example512.java
@@ -0,0 +1,30 @@
+package ChapterFive;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class example512 {
+    public static void main(String[] args) {
+        //declare a list that will contain all your files
+        //inside of a readme.txt file
+        List<String> lines = Collections.emptyList();
+
+        try {
+            //provoke an exception
+            lines = Files.readAllLines(Paths.get("readme.txt"));
+        } catch (NoSuchFileException fe) {
+            System.out.println("Exception: File Not Found");
+        } catch (IOException ioe) {
+            System.out.println("Exception: IOException");
+        } finally {
+            System.out.println("Exception: Case Closed");
+        }
+
+        //this print will never be seen
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
