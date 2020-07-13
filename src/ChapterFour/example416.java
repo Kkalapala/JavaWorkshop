36be9d9143c78593d4b19364726b2a3e79396460
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 18:22:25 +0530

    Added sorting to example

--- src/ChapterFour/example416.java
+++ src/ChapterFour/example416.java
@@ -3,6 +3,23 @@ package ChapterFour;
 import java.io.Console;
 import java.util.*;
 
+class DataPoint {
+    String key = "";
+    Integer value = 0;
+
+    //constructor
+    DataPoint(String s, Integer i) {
+        key = s;
+        value = i;
+    }
+}
+
+class SortByValue implements Comparator<DataPoint> {
+    //used for sorting ascending order
+    public int compare(DataPoint a, DataPoint b) {
+        return a.value - b.value;
+    }
+}
 public class example416 {
     public static void main(String[] args) {
         ArrayList <String> text = new ArrayList<String>();
@@ -23,6 +40,8 @@ public class example416 {
 
         Iterator iterator = textSet.iterator();
 
+        List <DataPoint> frequencies = new ArrayList<DataPoint>();
+
         while (iterator.hasNext()) {
             //point to next element
             String s = (String) iterator.next();
@@ -32,6 +51,22 @@ public class example416 {
 
             //print out the results
             System.out.println(s + " appears " + freq + " times");
+
+            //create the object to be stored
+            DataPoint dataPoint = new DataPoint(s, freq);
+
+            //add datapoints to the list
+            frequencies.add(dataPoint);
+        }
+
+        Collections.sort(frequencies,
+                Collections.reverseOrder(new SortByValue()));
+
+        System.out.println("Results sorted");
+        for (int i = 0; i < frequencies.size(); i++) {
+            System.out.println(frequencies.get(i).value
+            + " times for word "
+            + frequencies.get(i).key);
         }
     }
 }
