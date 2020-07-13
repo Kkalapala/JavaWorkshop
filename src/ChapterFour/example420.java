4428cdb5544d04aa829d617b089effdcca7e33b0
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 17:43:50 +0530

    iterate through map

---
+++ src/ChapterFour/example420.java
@@ -0,0 +1,22 @@
+package ChapterFour;
+
+import java.util.*;
+
+public class example420 {
+    public static void main(String[] args) {
+        Map map = new HashMap ();
+        map.put("name", "Kristian");
+        map.put("family name", "Larssen");
+        map.put("address", "Jumping Rd");
+        map.put("mobile", "555-12345");
+        map.put("pet", "cat");
+
+        Iterator<Map.Entry> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry entry = iterator.next();
+            System.out.print("Key = " + entry.getKey());
+            System.out.println(", Value = " + entry.getValue());
+        }
+    }
+}
