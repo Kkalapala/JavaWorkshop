8fba1513528fe25f89b4cd9c777e32ffa6cf8fea
kkalapala <kavya.kalapala@cloudium.io>
2020-07-13 20:28:07 +0530

    throw RuntimeException

---
+++ src/ChapterFive/example511.java
@@ -0,0 +1,14 @@
+package ChapterFive;
+
+public class example511 {
+    public static void main(String[] args) {
+        String text = null;
+
+        try {
+            System.out.println(text.length());
+        } catch (Exception e) {
+            System.out.println("Exception: this should be a NullPointerException");
+            throw new RuntimeException();
+        }
+    }
+}
