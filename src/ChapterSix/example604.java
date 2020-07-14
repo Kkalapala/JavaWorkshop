eb3565465dc3c271609f2664d5eb3f01cc6465c8
kkalapala <kavya.kalapala@cloudium.io>
2020-07-14 11:38:16 +0530

    static imports

---
+++ src/ChapterSix/example604.java
@@ -0,0 +1,9 @@
+package ChapterSix;
+
+import java.time.LocalDateTime;
+
+public class example604 {
+    public static void main(String[] args) {
+        System.out.println("The end of time is : " + LocalDateTime.MAX);
+    }
+}
