3f0c4f0a2d71667d2018d8762a56618b9aa6f1a7
kkalapala <kavya.kalapala@cloudium.io>
2020-07-15 17:29:37 +0530

    encrypting the string using AES

---
+++ src/ChapterTen/example101.java
@@ -0,0 +1,39 @@
+package ChapterTen;
+
+import javax.crypto.*;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+public class example101 {
+    public static void main(String[] args) {
+        try {
+            String algorithm = "AES";
+            Key privateKey = KeyGenerator.getInstance(algorithm).generateKey();
+
+            String transformation = algorithm + "/ECB/NoPadding";
+
+            Cipher cipher = Cipher.getInstance(transformation);
+
+            cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+            String plaintext = "My secret message";
+            byte [] ciphertext = new byte[cipher.getOutputSize(plaintext.getBytes().length)];
+            int encryptedLength = cipher.update(plaintext.getBytes(), 0, plaintext.getBytes().length, encrypted);
+            cipher.doFinal(ciphertext, encryptedLength);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (ShortBufferException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
