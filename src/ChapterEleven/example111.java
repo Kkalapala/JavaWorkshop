d41db2629d3c8a8e3d7faf111ffccb6b10a33cb7
kkalapala <kavya.kalapala@cloudium.io>
2020-07-15 18:04:44 +0530

    intro to processes

---
+++ src/ChapterEleven/example111.java
@@ -0,0 +1,11 @@
+package ChapterEleven;
+
+public class example111 {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println("Processors: " + runtime.availableProcessors());
+        System.out.println("Total memory: " + runtime.totalMemory());
+        System.out.println("Free memory: " + runtime.freeMemory());
+    }
+}
