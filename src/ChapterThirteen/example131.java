05f3b12e40dee714e725435d6204c0f714011c95
kkalapala <kavya.kalapala@cloudium.io>
2020-07-16 18:41:17 +0530

    pure functions

---
+++ src/ChapterThirteen/example131.java
@@ -0,0 +1,13 @@
+package ChapterThirteen;
+
+public class example131 {
+    public static void main(String[] args) {
+        System.out.println(sum(2,3));
+        System.out.println(sum(2,3));
+        System.out.println(sum(2,3));
+    }
+
+    static int sum(int price1, int price2) {
+        return price1 + price2;
+    }
+}
