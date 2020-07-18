cae15e7ad974d91eeaa2c1373e06aaebbfd75c80
kkalapala <kavya.kalapala@cloudium.io>
2020-07-18 17:07:58 +0530

    long count

---
+++ src/ChapterFifteen/StreamOperations/Count.java
@@ -0,0 +1,11 @@
+package ChapterFifteen.StreamOperations;
+
+import java.util.stream.Stream;
+
+public class Count {
+    public static void main(String[] args) {
+        long count = Stream.of(6, 3, 8, 12, 3, 9).count();
+        System.out.println(count);
+    }
+
+}
