2db77c86c32ef5ad6ab621cb86ccca9af6bceb84
kkalapala <kavya.kalapala@cloudium.io>
2020-07-18 17:14:26 +0530

    find any

---
+++ src/ChapterFifteen/StreamOperations/FindAny.java
@@ -0,0 +1,13 @@
+package ChapterFifteen.StreamOperations;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class FindAny {
+    public static void main(String[] args) {
+
+        Optional firstElement = Stream.of(7, 9, 3, 4, 1).findAny();
+        System.out.println(firstElement);
+
+    }
+}
