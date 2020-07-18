271deeb22c610e969897522fb3279029fcfd9dfc
kkalapala <kavya.kalapala@cloudium.io>
2020-07-18 17:12:01 +0530

    filter

---
+++ src/ChapterFifteen/StreamOperations/Filter.java
@@ -0,0 +1,13 @@
+package ChapterFifteen.StreamOperations;
+
+import java.util.stream.Stream;
+
+public class Filter {
+    public static void main(String[] args) {
+
+        Stream.of(1, 2, 3, 4, 5, 6)
+                .filter(i -> i > 3)
+                .forEach(System.out::println);
+
+    }
+}
