a13740de5ce4c9f8eaa23b28dc26ecc042d58e63
kkalapala <kavya.kalapala@cloudium.io>
2020-07-18 11:26:13 +0530

    Averaging int

---
+++ src/ChapterFifteen/StreamOperations/AveragingInt.java
@@ -0,0 +1,17 @@
+package ChapterFifteen.StreamOperations;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AveragingInt {
+    public static void main(String[] args) {
+        double average = Stream.of(1d, 2d, 3d)
+                .collect(Collectors.averagingInt(
+                        (d) -> {
+                            return d.intValue();
+                        }
+                ));
+
+        System.out.println(average);
+    }
+}
