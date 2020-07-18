36f01a19eb1430a8e053f000dd3c391beb1874ed
kkalapala <kavya.kalapala@cloudium.io>
2020-07-18 11:38:41 +0530

    averaging long

---
+++ src/ChapterFifteen/StreamOperations/AveragingLong.java
@@ -0,0 +1,18 @@
+package ChapterFifteen.StreamOperations;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AveragingLong {
+    public static void main(String[] args) {
+
+        double average = Stream.of(1d, 2d, 3d)
+                .collect(Collectors.averagingLong(
+                        (d) -> {
+                            return d.longValue();
+                        }
+                ));
+        
+        System.out.println(average);
+    }
+}
