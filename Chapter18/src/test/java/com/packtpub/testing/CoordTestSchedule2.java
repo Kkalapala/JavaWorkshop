a9dda1c41cf17f6f090ff00f36d9b5ac8f1ef8a6
kkalapala <kavya.kalapala@cloudium.io>
2020-07-20 18:17:38 +0530

    75% successful test

---
+++ Chapter18/src/test/java/com/packtpub/testing/CoordTestSchedule2.java
@@ -0,0 +1,56 @@
+package com.packtpub.testing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class CoordTestSchedule2 {
+    @Test
+    @DisplayName("Frequency must be less than 1440")
+    void testFrequency () {
+        CoordSchedule schedule = new CoordSchedule(
+                "2020-12-15T15:32Z",
+                "2020-12-30T05:15Z",
+                60
+        );
+
+        int frequency = schedule.getFrequency();
+        Assertions.assertTrue(frequency < CoordSchedule.MAX_FREQUENCY);
+        Assertions.assertTrue(frequency > CoordSchedule.MIN_FREQUENCY);
+    }
+
+    @Test
+    @DisplayName("Timestamp will be null if not formatted correctly")
+    void testStartingTimestamps () {
+        CoordSchedule schedule = new CoordSchedule(
+                "2020/12/15T15:32Z",
+                "2020/12/30T05:15Z",
+                60
+        );
+
+        Date starting = schedule.getStartingTimestampAsDate();
+
+        //Timestamp not formatted properly
+        Assertions.assertNull(starting);
+    }
+
+    @Test
+    @DisplayName("Ending timestamp must be after starting")
+    void testTimestamps () {
+        CoordSchedule schedule = new CoordSchedule(
+                "2020-12-15T15:32Z",
+                "2020-12-15T15:35Z",
+                60
+        );
+
+        Date starting = schedule.getStartingTimestampAsDate();
+        Assertions.assertNotNull(starting);
+
+        Date ending = schedule.getEndingTimestampAsDate();
+        Assertions.assertNotNull(ending);
+
+        Assertions.assertTrue(ending.after(starting));
+    }
+}
