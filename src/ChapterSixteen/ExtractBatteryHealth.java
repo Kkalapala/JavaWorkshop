8584cf41dfae4c2aa48bd5371e3e860bc945b273
kkalapala <kavya.kalapala@cloudium.io>
2020-07-20 12:25:26 +0530

    using function

---
+++ src/ChapterSixteen/ExtractBatteryHealth.java
@@ -0,0 +1,10 @@
+package ChapterSixteen;
+
+import java.util.function.Function;
+
+public class ExtractBatteryHealth implements Function<Sensor, Integer> {
+    @Override
+    public Integer apply(Sensor sensor) {
+        return sensor.batteryHealth();
+    }
+}
