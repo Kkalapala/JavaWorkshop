d874934d5b6d73817ae35809f17fb5d30ab1c1e5
kkalapala <kavya.kalapala@cloudium.io>
2020-07-22 17:27:59 +0530

    example 3

---
+++ src/ChapterTwenty/example3.java
@@ -0,0 +1,17 @@
+package ChapterTwenty;
+
+import java.util.Optional;
+
+public class example3 {
+    public static void main(String[] args) {
+        // non nullable construction
+        Optional<String> textNonNullable = Optional.of("not nullable");
+        String textNonNullableSafe = textNonNullable.orElse("this is empty");
+        System.out.println("Non-Empty Optional:" + textNonNullableSafe);
+
+        // the empty object
+        Optional<String> textEmpty = Optional.empty();
+        String textEmptySafe = textEmpty.orElse("this is empty");
+        System.out.println("Empty Optional: " + textEmptySafe);
+    }
+}
