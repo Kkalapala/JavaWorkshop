8faa4537fa47d1eccdb0e3eecca080eb62906205
kkalapala <kavya.kalapala@cloudium.io>
2020-07-22 17:24:14 +0530

    optional examples

---
+++ src/ChapterTwenty/example2.java
@@ -0,0 +1,15 @@
+package ChapterTwenty;
+
+import java.util.Optional;
+
+public class example2 {
+    public static void main(String[] args) {
+        // non nullable construction
+        Optional<String> textNonNullable = Optional.of("not nullable");
+
+        System.out.println("Non-Empty Optional:" + textNonNullable.get());
+        // the empty object
+        Optional<String> textEmpty = Optional.empty();
+        System.out.println("Empty Optional: " + textEmpty.get());
+    }
+}
