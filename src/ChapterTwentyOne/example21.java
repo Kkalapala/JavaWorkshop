27030ebcee1e88ac261d33e11b3dc7dafb265daf
kkalapala <kavya.kalapala@cloudium.io>
2020-07-23 17:54:23 +0530

    calling weak references

---
+++ src/ChapterTwentyOne/example21.java
@@ -0,0 +1,33 @@
+package ChapterTwentyOne;
+
+import java.lang.ref.WeakReference;
+
+public class example21 {
+    public static void main(String[] args) {
+        MyObject myObject = new MyObject();
+        System.out.println(myObject);
+
+        WeakReference<MyObject> weakReference = new WeakReference<>(myObject);
+
+        MyObject myObject2 = weakReference.get();
+        System.out.println(myObject2);
+
+        myObject = null;
+        myObject2 = null;
+
+        System.gc();
+        System.runFinalization();
+
+        MyObject myObject3 = weakReference.get();
+        System.out.println(myObject3);
+    }
+}
+
+    class MyObject {
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("Finalize called");
+        }
+    }
+
